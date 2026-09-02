906bdd9949500bfce1e0eff6fcae6111a32f0775
MariamAlix24 <alimariem59@gmail.com>
2026-09-02 12:24:11 +0300

    question 1

--- G-ASP-NET-99-C#Basics-02/Program.cs
+++ G-ASP-NET-99-C#Basics-02/Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace G_ASP_NET_99_C_Basics_02
 {
+    internal class Book
+    {
+        public string Title;
+        public int Pages;
+        public override string ToString()
+        {
+            return $" Book Title: {Title}, Pages: {Pages}";
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+            // Q1
+            Book b1= new Book();
+            b1.Title = "nany hunter";
+            b1.Pages = 300;
+            object obj = b1;
+            Console.WriteLine(obj);
+            #endregion
         }
     }
 }
